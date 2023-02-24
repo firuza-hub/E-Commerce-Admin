36dfb923e20bfb8cc7aafc561453ec7966d129a1
Firuza Aliyeva <firuza.aliyeva@procure.az>
2023-02-24 14:32:30 +0400

    product list image loading added with coil

--- app/src/main/java/az/red/e_commerce_admin_android/ui/screens/bottomnav/home/components/HomeProductListItem.kt
+++ app/src/main/java/az/red/e_commerce_admin_android/ui/screens/bottomnav/home/components/HomeProductListItem.kt
@@ -1,7 +1,6 @@
 package az.red.e_commerce_admin_android.ui.screens.bottomnav.home.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,12 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import az.red.e_commerce_admin_android.R
 import az.red.e_commerce_admin_android.data.remote.product.dto.response.ProductListItemResponse
 import az.red.e_commerce_admin_android.ui.themes.CustomTheme
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun ProductListItem(productListItemResponse: ProductListItemResponse) {
@@ -34,14 +37,14 @@ fun ProductListItem(productListItemResponse: ProductListItemResponse) {
                 .height(97.dp)
         ) {
 
-            ProductListImage()
+            productListItemResponse.imageUrls.let { if (it.any()) ProductListImage(it.first()) }
             ProductListInfo(productListItemResponse)
         }
     }
 }
 
 @Composable
-fun ProductListImage() {
+fun ProductListImage(imgUrl: String) {
     Box(
         modifier = Modifier
             .size(width = 97.dp, height = 97.dp)
@@ -49,9 +52,12 @@ fun ProductListImage() {
             .background(CustomTheme.colors.btnText)
     ) {
 
-        Image(
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imgUrl)
+                .crossfade(true)
+                .build(),
             modifier = Modifier.fillMaxSize(),
-            painter = painterResource(id = R.drawable.productlist_item_image),
             contentDescription = "productListItemImage",
             contentScale = ContentScale.Fit
         )
